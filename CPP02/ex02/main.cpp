11652063466088e8601ed26f192298488b862eac
kissyass <kissyass@mail.ru>
2023-11-28 23:48:39 +0300

    update ex02 cpp02

--- CPP02/ex02/main.cpp
+++ CPP02/ex02/main.cpp
@@ -1,10 +1,5 @@
 #include "Fixed.h"
 
-/*
-   guide to Operator Overloading:
-    - https://stackoverflow.com/questions/4421706/what-are-the-basic-rules-and-idioms-for-operator-overloading
-*/
-
 int main ( void ) {
     Fixed        a;
     Fixed const  b( Fixed( 5.05f ) * Fixed( 2 ) );
