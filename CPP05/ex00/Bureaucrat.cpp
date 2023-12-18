5f8c697dd947d4cdc3954dd787d92004f08588e7
kissyass <kissyass@mail.ru>
2023-12-18 23:01:05 +0300

    cpp05 ex00

---
+++ CPP05/ex00/Bureaucrat.cpp
@@ -0,0 +1,58 @@
+#include "./Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(void) : _name("Default worker"), _grade(75)
+{
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &copy) : _name(copy._name)
+{
+    *this = copy;
+}
+
+Bureaucrat & Bureaucrat :: operator=(Bureaucrat const &rhs)
+{
+    if (this != &rhs)
+        this->_grade = rhs._grade;
+    
+    return *this;
+}
+
+Bureaucrat::~Bureaucrat(void)
+{
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name), _grade(grade)
+{
+    if (grade < 1)
+        throw Bureaucrat::GradeTooHighException();
+    if (grade > 150)
+        throw Bureaucrat::GradeTooLowException();
+}
+std::string Bureaucrat :: getName(void) const
+{
+    return this->_name;
+}
+
+int Bureaucrat :: getGrade(void) const
+{
+    return this->_grade;
+}
+
+void    Bureaucrat::incrementGrade() {
+    if ( _grade - 1 < 1 )
+        throw Bureaucrat::GradeTooHighException();
+    _grade--;
+}
+
+void    Bureaucrat::decrementGrade() {
+    if ( _grade + 1 > 150 )
+        throw Bureaucrat::GradeTooLowException();
+    _grade++;
+}
+
+std::ostream & operator<<(std::ostream &output, const Bureaucrat &worker)
+{
+    output << worker.getName() << ", bureaucrat grade " << worker.getGrade() << ".";
+
+    return output;
+}
---
+++ CPP05/ex00/main.cpp
@@ -0,0 +1,39 @@
+#include "./Bureaucrat.hpp"
+
+int main()
+{
+    try 
+    {
+        Bureaucrat bureaucrat("Ken", 1);
+
+        std::cout << bureaucrat << std::endl;
+
+        std::cout << "Incrementing grade" << std::endl;
+        bureaucrat.incrementGrade();
+        std::cout << "Decrementing grade" << std::endl;
+        bureaucrat.decrementGrade();
+    } 
+    catch (Bureaucrat::GradeTooHighException &e) 
+    {
+        std::cout << e.what() << std::endl;   
+    }
+
+    try 
+    {
+        Bureaucrat worker;
+
+        std::cout << worker << std::endl;
+
+        while (worker.getGrade() < 150)
+            worker.decrementGrade();
+        
+        std::cout << worker << std::endl;
+        std::cout << "Decrementing grade last time" << std::endl;
+        worker.decrementGrade();
+    } 
+    catch (Bureaucrat::GradeTooLowException &e) 
+    {
+        std::cout << e.what() << std::endl;   
+    }
+    return EXIT_SUCCESS;
+}
