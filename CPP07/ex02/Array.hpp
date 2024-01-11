9263fcee15c72b972670aed5628857ea5dd4c4ed
kissyass <kissyass@mail.ru>
2024-01-11 12:44:19 +0300

    cpp07 done

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -11,16 +11,19 @@ class Array
         unsigned int _size;
     public:
         Array(void) : _vals(NULL), _size(0) {}
+
         Array(Array const &copy) : _vals(new T[copy._size]), _size(copy._size)
         {
             for (unsigned int i = 0; i < copy._size; i++)
                 this->_vals[i] = copy._vals[i];
         }
+
         Array & operator=(Array const &rhs)
         {
             if (this != &rhs)
             {   
-                delete [] this->_vals;
+                if (this->_vals != NULL)
+                   delete [] this->_vals;
                 this->_size = rhs._size;
                 this->_vals = new T[this->_size];
                 for (unsigned int i = 0; i < rhs._size; i++)
@@ -28,20 +31,24 @@ class Array
             }
             return *this;
         }
+
         ~Array(void)
         {
-            if (this->_vals)
+            if (this->_vals != NULL)
                 delete [] this->_vals;
         }
+
         Array(unsigned int len) : _vals(new T[len]), _size(len) {}
-        Array & operator[](Array const &in)
+
+        T & operator[](unsigned int in) const
         {
             if (in >= this->_size)
                 throw InvalidIndex();
             return this->_vals[in];
         }
+
         unsigned int size(void) const
-        {return this->_size;}
+            {return this->_size;}
 
         class  InvalidIndex : public std::exception
         {
--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -3,14 +3,43 @@
 int main()
 {
     Array<int> arr;
-    Array<int> a(10);
 
     std::cout << "First array size: " << arr.size() << std::endl;
-    for (unsigned int i = 0; i < a.size(); i++)
-        a[i] = i;
-    std::cout << "Second array size: " << arr.size() << std::endl;
-    std::cout << "Second array size arguments: " << std::endl;
-    for (unsigned int i = 0; i < a.size(); i++)
-        std::cout << a[i] << std::endl;
+    std::cout << "-------------------------------------------------" << std::endl;
+    try
+    {
+        Array<int> a(10);
+
+        for (unsigned int i = 0; i < a.size(); i++)
+            a[i] = i;
+        std::cout << "Second array size: " << a.size() << std::endl;
+        std::cout << "Second array size arguments: " << std::endl;
+        for (unsigned int i = 0; i < a.size(); i++)
+            std::cout << a[i] << std::endl;
+        std::cout << "Trying to access 10th index" << std::endl;
+        std::cout << a[10];
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    std::cout << "-------------------------------------------------" << std::endl;
+    Array<char> c(5);
+
+    for (unsigned int i = 0; i < c.size(); i++)
+        c[i] = i + 97;
+    std::cout << "Char array size: " << c.size() << std::endl;
+    std::cout << "Char array size arguments: " << std::endl;
+    for (unsigned int i = 0; i < c.size(); i++)
+        std::cout << c[i] << std::endl;
+    std::cout << "-------------------------------------------------" << std::endl;
+    Array<std::string> s(5);
+
+    for (unsigned int i = 0; i < c.size(); i++)
+        s[i] = "Hello!";
+    std::cout << "String array size: " << s.size() << std::endl;
+    std::cout << "String array size arguments: " << std::endl;
+    for (unsigned int i = 0; i < s.size(); i++)
+        std::cout << s[i] << std::endl;
     return 0;
 }
